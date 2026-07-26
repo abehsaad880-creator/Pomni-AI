40271d81b1db174a47c052dd0b73780062968cd6
abehsaad880-creator <abehsaad880@gmail.com>
2026-07-26 12:29:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ import sub from './sub.js';
 /* =========== Client ========== */
 const client = new Client({
   phoneNumber: '26301469', // Bot number
+  
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
   // Owner 1
     { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "22243493820@s.whatsapp.net" },
+    { name: "emam", lid: "221307316789354@lid", jid: "22242594277@s.whatsapp.net" },
   // Owner 3
     { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
